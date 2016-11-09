315683080ec4eae9718299174a44870b8d5845b2
Behzad Azampour <behy1368@gmail.com>
2016-11-09 22:48:46 +0000

    working version of tinyURL with delete and update included

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
+var urlExists = require('url-exists');
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -41,8 +43,7 @@ var urlDatabase = {
 
 
 app.get("/", (req, res) => {
-  //res.end("Hello");
-  //res.send("index");
+  console.log("--> inside get(/)");
   res.render("home/index");
 });
 
@@ -51,44 +52,76 @@ app.get("/", (req, res) => {
 // });
 
 
-// app.get("/urls", (req, res) => {
-//   let templateVars = { urls: urlDatabase }
-//   res.render("urls_index", templateVars);
-// });
+app.get("/urls", (req, res) => {
+  console.log("--> inside get(/urls)");
+  let templateVars = { urls: urlDatabase, statusMessage: "" }
+  res.render("urls/index", templateVars);
+});
 
-// app.get("/urls/new", (req, res) => {
-//   res.render("urls_new");
+app.get("/urls/new", (req, res) => {
+  console.log("--> inside get(/urls/new)");
+  res.render("urls/new");
 
-// });
+});
 
-// app.get("/urls/:id", (req, res) => {
-//   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
-//   console.log(urlDatabase[req.params.id]);
-//   res.render("urls_show", templateVars);
-// });
+app.get("/urls/:id", (req, res) => {
+  console.log("--> inside get(/urls/:id)");
+  if (urlDatabase[req.params.id] !== undefined) {
+    let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls/show", templateVars);
+  } else {
+    let templateVars = { urls: urlDatabase, msg: "No matching short URL found.", statusMessage: ""};
+    res.render("urls/index", templateVars);
+  }
+});
 
 // app.get("/test", (req, res) => {
 //   res.render("urls");
 // });
 
-// app.post("/urls", (req, res) => {
-//   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-//   let rndmStr = randomString(6);
-//   let tempUrl = req.body.longURL;
-//   if (!tempUrl.includes("http://",0) && !tempUrl.includes("https://")) {
-//     tempUrl = `http://${tempUrl}`;
-//   }
-//   urlDatabase[rndmStr] = tempUrl;
-//   res.redirect("/urls/new?success=true");
-// });
+app.post("/urls", (req, res) => {
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log("--> inside post(/urls)");
+  let rndmStr = randomString(6);
+  let tempUrl = req.body.longURL;
+  let statusMessage = "";
+  // if (!tempUrl.includes("http://",0) && !tempUrl.includes("https://")) {
+  //   tempUrl = `http://${tempUrl}`;
+  // }
 
-// app.get("/u/:shortURL", (req, res) => {
-//   if (urlDatabase[req.params.shortURL] !== undefined) {
-//     let longURL = urlDatabase[req.params.shortURL];
-//     res.redirect(longURL);
-//   }
-//   else res.redirect('/urls');
-// });
+  // urlExists(tempUrl, (err, exists) => {
+  //   statusMessage = exists ? "Short url added, page exists." : "Short url added, page does not exist.";
+  //   urlDatabase[rndmStr] = tempUrl;
+  //   res.redirect(`/urls?${statusMessage}`);
+  // });
+  checkNewUrlAndAdd(tempUrl,rndmStr,"/urls",res);
+});
+
+
+app.post("/urls/*/delete", (req,res) => {
+  console.log("--> inside post(/urls/*/delete");
+  delete urlDatabase[req.params['0']];
+  res.redirect("/urls");
+});
+
+app.post("/urls/*/edit", (req,res) => {
+  console.log("--> inside post(/urls/*/edit");
+  // urlDatabase[req.params['0']] = req.body.longURL;
+  // res.redirect("/urls");
+
+  checkNewUrlAndAdd(req.body.longURL,req.params['0'],"/urls",res);
+});
+
+
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log("--> inside get(/u/:shortURL)");
+  if (urlDatabase[req.params.shortURL] !== undefined) {
+    let longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  }
+  else res.redirect('/urls/index');
+});
 
 // app.get("/hello", (req, res) => {
 //   res.end("<html><body>Hello <b>World</b> </body> </html> \n");
@@ -98,9 +131,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// app.get("*", function(req, res){
-//   res.send('what???', 404);
-// });
+app.get("*", function(req, res){
+  res.send('what???', 404);
+});
 
 function randomString(length) {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -108,6 +141,24 @@ function randomString(length) {
   for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 }
+
+
+function checkNewUrlAndAdd (url, urlDataBaseKey, redirectUrl, res) {
+  if (!url.includes("http://",0) && !url.includes("https://")) {
+    url = `http://${url}`;
+  }
+
+  urlExists(url, (err, exists) => {
+    // statusMessage = exists ? "Short url added, page exists." : "Short url added, page does not exist.";
+    if (exists) {
+      urlDatabase[urlDataBaseKey] = url;
+      //res.redirect(`/urls?${statusMessage}`);
+      res.redirect(redirectUrl);
+    } else res.redirect(redirectUrl);
+
+  });
+}
+
 
 // function generateRandomString() {
 //   //rndmStr = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
